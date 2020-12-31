2ab8cafefe8938f8baa7753a878c2c1dc0c0fcff
Clemens Gogoljn <service@cylancer.net>
2020-12-31 17:01:07 +0100

    add comments

--- bypassemailsendlimitation/Classes/XClass/CyMailMessage.php
+++ bypassemailsendlimitation/Classes/XClass/CyMailMessage.php
@@ -18,7 +18,9 @@ class CyMailMessage extends \TYPO3\CMS\Core\Mail\MailMessage
             parent::send();
         } catch (\Exception $e) {
             try {
+                // The destroying of the transport queue starts the send process...  
                 $this->mailer->getTransport()->__destruct();
+                // after the sending you can try to send your message. 
                 parent::send();
             } catch (\Exception $e) {
                 $this->sent = false;
