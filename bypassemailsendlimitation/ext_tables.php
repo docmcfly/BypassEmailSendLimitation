a690b68f5b3c0f4792bf2f2bc7edc58d1ae72492
Clemens Gogoljn <service@cylancer.net>
2021-01-01 12:26:34 +0100

    clean & update

--- bypassemailsendlimitation/ext_tables.php
+++ bypassemailsendlimitation/ext_tables.php
@@ -1,11 +1,2 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
-
-call_user_func(
-    function()
-    {
-
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('bypassemailsendlimitation', 'Configuration/TypoScript', 'Bypass email send limitation');
-
-    }
-);
