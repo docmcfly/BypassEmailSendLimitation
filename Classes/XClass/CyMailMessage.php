483fc34ebf1c54763122965976cba2030c68e463
Clemens Gogolin <service@cylancer.net>
2021-01-04 13:29:00 +0100

    catch better a transport exception as a general exception

--- Classes/XClass/CyMailMessage.php
+++ Classes/XClass/CyMailMessage.php
@@ -2,25 +2,27 @@
 namespace Cylancer\Bypassemailsendlimitation\XClass;
 
 
+use Symfony\Component\Mailer\Exception\TransportException;
+
 /**
  * Extension Bypass email send limitation
  */
 class CyMailMessage extends \TYPO3\CMS\Core\Mail\MailMessage
 {
-
+    
     /**
-     * @Override 
+     * @Override
      * @return bool whether the message was accepted or not
      */
     public function send(): bool
     {
         try {
             parent::send();
-        } catch (\Exception $e) {
+        } catch (TransportException $e) {
             try {
-                // The destroying of the transport queue starts the send process...  
+                // The destroying of the transport queue starts the send process...
                 $this->mailer->getTransport()->__destruct();
-                // after the sending you can try to send your message. 
+                // after the sending you can try to send your message again.
                 parent::send();
             } catch (\Exception $e) {
                 $this->sent = false;
